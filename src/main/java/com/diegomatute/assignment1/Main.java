e8d7735f1dcf117c09706ca6c93a278b4d99c2d3
diegomatute <Dmatute43@gmail.com>
2020-09-13 18:00:29 -0400

    First comit

---
+++ src/main/java/com/diegomatute/assignment1/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diegomatute.assignment1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author diegomatute
+ */
+public class Main { public static void main(String args[]) throws FileNotFoundException
+{
+    String inFile = "inputFile.txt";
+    String outFile;
+    
+    Scanner input = new Scanner(System.in);
+    System.out.print(inFile);
+    System.out.printf("Enter input file name: ");
+    inFile = input.nextLine(); //.trim();
+    System.out.print(inFile);
+    //Scanner inputScanner;
+    //inputScanner = new Scanner(new FileReader(filename));
+    
+   // FileReader fr = new FileReader(filename);
+    //Scanner infile = new Scanner(fr);
+    //File input = new File(inFile);
+    
+    
+    
+    
+    
+    
+    
+}
+    
+    
+    
+}
